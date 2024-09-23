9384cf4ed8172c34315e4819362788cf8fdc3d29
Rashed Al Ghailani <Authority217@gmail.com>
2024-09-23 09:42:44 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dictionary_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
